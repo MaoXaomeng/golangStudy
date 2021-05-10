95844f2f790368c40701c88057dd1e644c2b8697
maohuatang <2018023060@email.szu.edu.cn>
2021-05-10 10:29:30 +0800

    for字符串循环用range而非索引表示

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import (
 )
 
 func main() {
-	//	xCapital := make(chan byte)
-	x := "hello!"
-	for i := 0; i < len(x); i++ {
-		x := x[i]
-		if x != '!' {
-			x := x + 'A' - 'a'
-			//			xCapital <- x
-			fmt.Printf("%c", x)
-		}
+	x := "hello"
+	for _, x := range x {
+		x := x + 'A' - 'a'
+		fmt.Printf("%c", x)
 	}
 }
