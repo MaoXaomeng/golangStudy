4417e00c72f0e79248eeee7f2db4d66a31dc76a0
maohuatang <2018023060@email.szu.edu.cn>
2021-05-13 23:19:11 +0800

    利用指针实现结构方法和方程

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println(double(4))
+	p := Point{1, 1}
+	q := Point{4, 5}
+	fmt.Println(Distance(&q, &p))
+	fmt.Println((&q).Distance(&p))
 }
-func double(x int) (result int) {
-	defer func() {
-		result += x
-		fmt.Printf("double(%d) = %d\n", x, result)
-	}()
-	result = x + x
-	return
+
+type Point struct{ X, Y float64 }
+
+func Distance(p, q *Point) float64 {
+	return math.Hypot((*p).X-(*q).X, (*p).Y-(*q).Y)
+}
+
+func (p *Point) Distance(q *Point) float64 {
+	return math.Hypot((*p).X-(*q).X, (*p).Y-(*q).Y)
 }
