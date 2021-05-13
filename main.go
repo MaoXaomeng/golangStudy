338c70e4bad633f8a6c76e2d8ebe8d5d392721c5
maohuatang <2018023060@email.szu.edu.cn>
2021-05-13 08:50:07 +0800

    遍历url中的超链接

--- main.go
+++ main.go
@@ -2,20 +2,78 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io"
+	"net/http"
+	"os"
 )
 
+//设置节点类型
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+//设置节点属性
+type Attribute struct {
+	Key, Val string
+}
+
+//设置节点
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
 func main() {
-	type Employee struct {
-		ID        int
-		Name      string
-		Address   string
-		DoB       time.Time
-		Position  string
-		Salary    int
-		ManagerID int
+
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2:%v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
 	}
-	var dilbert Employee
-	dilbert.Salary = 80000
-	fmt.Println(dilbert)
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
+}
+func Parse(r io.Reader) (*Node, error)
+func visit(links []string, n *Node) []string {
+	if n.Type == ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	//递归式查找子节点
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
 }
