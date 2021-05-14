a616a9c77e09f1d6ed9a4de4116f3ac0390df774
maohuatang <2018023060@email.szu.edu.cn>
2021-05-14 10:21:00 +0800

    结构方法的使用[]Point类型 path

--- main.go
+++ main.go
@@ -6,18 +6,30 @@ import (
 )
 
 func main() {
-	p := Point{1, 1}
-	q := Point{4, 5}
-	fmt.Println(Distance(&q, &p))
-	fmt.Println((&q).Distance(&p))
+	perim := Path{
+		{1, 1},
+		{5, 2},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println((&perim).Distance())
+
 }
 
 type Point struct{ X, Y float64 }
 
-func Distance(p, q *Point) float64 {
-	return math.Hypot((*p).X-(*q).X, (*p).Y-(*q).Y)
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(p.X-q.X, p.Y-q.Y)
 }
 
-func (p *Point) Distance(q *Point) float64 {
-	return math.Hypot((*p).X-(*q).X, (*p).Y-(*q).Y)
+type Path []Point
+
+func (path *Path) Distance() float64 {
+	var sum float64
+	for i := range *path {
+		if i > 0 {
+			sum += (*path)[i-1].Distance((*path)[i])
+		}
+	}
+	return sum
 }
